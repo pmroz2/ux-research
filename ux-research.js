ecff67aa748f2b24374fc4f517197d84e33f1606
pmroz2 <pmroz@trans.eu>
2024-12-02 17:09:02 +0100

    Update ux-research.js

--- ux-research.js
+++ ux-research.js
@@ -34,3 +34,17 @@
         }
     }
 })();
+
+
+console.log('Skrypt został załadowany z Amazon S3.');
+
+document.addEventListener('click', function(event) {
+    var taskElement = event.target.closest('[data-label="task-completed"]');
+    if (taskElement) {
+        console.log('Kliknięto element z data-label="task-completed":', taskElement);
+    } else {
+        console.log('Kliknięto inny element:', event.target);
+    }
+}, { passive: false });
+
+
