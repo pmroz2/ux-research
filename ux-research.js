2cb838c193c21f2b6fc5eafd208d9f5bdd374ac5
pmroz2 <pmroz@trans.eu>
2024-12-02 14:56:51 +0100

    Update ux-research.js

--- ux-research.js
+++ ux-research.js
@@ -3,7 +3,7 @@ console.log('Skrypt został załadowany z Github.1');
 document.addEventListener('click', function(event) {
     var taskElement = event.target.closest('[data-label="task-completed"]');
     if (taskElement) {
-        console.log('Kliknięto element z data-label="task-completed":', taskElement);
+        console.log('Kliknięto element 3 z data-label="task-completed":', taskElement);
     } else {
         console.log('Kliknięto inny element:', event.target);
     }
