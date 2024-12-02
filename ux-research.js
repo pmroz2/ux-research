c43106ec218eaa5e72e39075daefc391249191a4
pmroz2 <pmroz@trans.eu>
2024-12-02 14:27:19 +0100

    Add files via upload

---
+++ ux-research.js
@@ -0,0 +1,10 @@
+console.log('Skrypt został załadowany z Dropbox.');
+
+document.addEventListener('click', function(event) {
+    var taskElement = event.target.closest('[data-label="task-completed"]');
+    if (taskElement) {
+        console.log('Kliknięto element z data-label="task-completed":', taskElement);
+    } else {
+        console.log('Kliknięto inny element:', event.target);
+    }
+}, { passive: false });
